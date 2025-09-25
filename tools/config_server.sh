8bb93336372cdaf43d16baf65af0d9212e4bbcad
Alejandro <alejandrolopezfuentes789@gmail.com>
2025-09-25 07:08:03 +0000

    update

--- tools/config_server.sh
+++ tools/config_server.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-seed=3868635343581
-motd="§6§l✦ §aBienvenido a §eHISPANOZUELA §6§l✦§r"
-
 # Set EULA to true
 sed -i 's/eula=false/eula=true/' /server/eula.txt
 
 # Set MOTD
-sed -i 's/^motd=.*/motd=$motd/' /server/server.properties
+sed -i 's/^motd=.*/motd=§6§l✦ §aBienvenido a §eHISPANOZUELA §6§l✦§r/' /server/server.properties
 
 # Disable online mode
 sed -i 's/^online-mode=.*/online-mode=false/' /server/server.properties
 
 # Set world seed
-sed -i 's/^level-seed=.*/level-seed=$seed/' /server/server.properties
+sed -i 's/^level-seed=.*/level-seed=3868635343581/' /server/server.properties
 
 # Copy logo
 cp /server-icon.png /server
